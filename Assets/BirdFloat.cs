e75b4c02837d8b38d92f8c7372aa23f4aafc95e8
Justine Zape <y3856547j@iespoblenou.org>
2018-04-24 20:24:08 +0200

    FlyBirdyFly commit
    birdy getting the coin, updating the score, the particle system of coin destroying each particle,

---
+++ Assets/BirdFloat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdFloat : MonoBehaviour {
+
+    public float speed;
+    
+    private int coins;
+    public Text text;   
+
+    void Start()
+    {
+        coins = 0;
+        updateCounter();
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        GetComponent<Rigidbody2D>().AddForce(direction * speed);
+
+      
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        //if (other.CompareTag("coins"))
+          //  Destroy(other.gameObject);
+        coins++;
+        updateCounter();
+
+    }
+
+    void updateCounter()
+    {
+        text.text = "score: " + coins;
+    }
+}
---
+++ Assets/Coins.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour {
+
+    private ParticleSystem ps;
+
+    
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+
+    }
+
+    void Update()
+    {
+        // initialize an array the size of our current particle count
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.particleCount];
+        // *pass* this array to GetParticles...
+        int num = ps.GetParticles(particles);
+        Debug.Log("Found " + num + " active particles.");
+        /*for (int i = 0; i < num; i++)
+        {
+            if (particles[i].position.z > 5) // large local z: let particle drop down
+                particles[i].velocity -= Vector3.up * 0.1f;
+        }
+        // re-assign modified array
+        ps.SetParticles(particles, num);*/
+    }
+}
